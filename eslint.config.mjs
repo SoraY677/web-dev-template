5b4b3838335d7048d1c1cd4e13a1ad8c2e7dd0a8
soray677 <soray677@outlook.com>
2025-04-24 22:32:13 +0900

    eslint拡充

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,65 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import unicorn from 'eslint-plugin-unicorn';
+
+const targetExt = 'ts,tsx,vue'
+const targetFiles = `**/*.{${targetExt}}`
 
 export default defineConfig([
+  // ファイル内容制約
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    'rules': {
-      'no-extra-semi': 'warn',
+    files: [targetFiles],
+    rules: {
+      semi: ['error', 'never'],
+      'semi-spacing': ['error', {after: true, before: false}],
+      'quote-props': ['error', 'as-needed'],
+      'no-extra-semi': 'error',
       'no-undef': 'warn',
-      'quotes': ['error', 'single'],
-      'space-before-blocks': ['warn', { 'functions': 'always' }]
-    }
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+      quotes: ['error', 'single'],
+      'space-before-blocks': ['error', { functions: 'always' }],
+      'semi-style': ['error', 'last'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': 'error',
+      'array-bracket-spacing': 'error',
+      'block-spacing': 'error',
+      'brace-style': 'error',
+      'camelcase': 'error',
+      'comma-spacing': 'error',
+      'comma-style': 'error',
+      'computed-property-spacing': 'error',
+      'consistent-this': 'error',
+      'eol-last': 'error',
+      'indent': 'error',
+      // 'key-spacing': 'error',
+      'lines-around-comment': 'error',
+      'no-mixed-spaces-and-tabs': 'error',
+      'no-multiple-empty-lines': 'error',
+      'no-nested-ternary': 'error',
+      'no-trailing-spaces': 'error',
+      'operator-linebreak': 'error',
+      'sort-vars': 'error',
+      'keyword-spacing': ["error", {"before": true, "after": true }],
+      'space-infix-ops': 'error',
+    },
     languageOptions: { globals: globals.browser },
   },
-  
+  // ファイル名の制約
+  {
+    files: [targetFiles],
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            pascalCase: true
+          },
+        },
+      ],
+    },
+  },
   tseslint.configs.recommended,
-]);
+])
