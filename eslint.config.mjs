a75444f492cf3e09346118bca912f579e8d73d46
soray677 <soray677@outlook.com>
2025-04-24 21:15:23 +0900

    husky add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
+    'rules': {
+      'no-extra-semi': 'warn',
+      'no-undef': 'warn',
+      'quotes': ['error', 'single'],
+      'space-before-blocks': ['warn', { 'functions': 'always' }]
+    }
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: globals.browser },
   },
+  
   tseslint.configs.recommended,
 ]);
